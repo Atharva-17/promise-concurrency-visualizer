b7f4347bf339e068037d2ae2c96644b9bca3b057
Atharva-17 <atharvamanish50@gmail.com>
2024-04-10 17:23:32 +0530

    chore: minor css changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
 			>
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
-						<main className="container mx-auto max-w-7xl px-6 flex-grow">
+						<main className="container mx-auto max-w-7xl px-6">
 							{children}
 						</main>
 						
